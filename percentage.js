d63b0b9f58bb697aaa4a664e0f1fd543948ee0a5
Salvatore-Malanchino <malanchinos@hotmail.it>
2023-12-11 17:02:30 +0100

    Circle

--- percentage.js
+++ percentage.js
@@ -3,6 +3,7 @@ const showResultBox = () => {
   scoreText = `Your score${userScore} out of ${questions.length}`;
 
   const progressValue = document.getElementsByClassName('progress-value');
+  const circular = document.getElementsByClassName('circular');
   let progressStartValue = 0;
   let progressEndValue = (userScore / questions.lenght) * 100;
   let speed = 15;
@@ -10,8 +11,12 @@ const showResultBox = () => {
   let progress = setInterval(() => {
     progressStartValue++;
     progressValue.textContent = ` ${progressStartValue} % `;
+    let circularStyle = (circular.style.color = `#00FFFF ${
+      progressStartValue * 3.6
+    } `);
     if (progressStartValue === progressEndValue) {
       clearInterval(progress);
     }
   });
 };
+console.log(showResultBox());
