8468c7d00380daabb21bd5a0c9f9ed3a4741fb52
Adriano Bruzzese <146066663+adrianobruzzese@users.noreply.github.com>
2023-12-12 17:15:33 +0100

    fixed-star-rating-system.js

--- script/stars-rating.js
+++ script/stars-rating.js
@@ -1,15 +1,15 @@
-const stars = document.querySelectorAll("img.star");
-const divStars = document.getElementById("stars");
-let rating = 0;
+const stars = document.querySelectorAll("img.star"); //target elementi con classe "star"
+const divStars = document.getElementById("stars"); //target container con id stars
+let rating = 0; //inizializzazione
 
 stars.forEach((star) => {
   star.addEventListener("mouseover", (event) => {
     const hoveredStarIndex = parseInt(event.target.getAttribute("data-index"));
-    // Se l'indice della stella su cui si mette il cursore è superiore all'indice della stella già selezionata (se esistente), le stelle diventano piene fino al cursore
+    // Se l'indice della stella su cui si mette il cursore è superiore all'indice della stella già selezionata (se esistente), le stelle diventano piene fino al cursore.
     if (hoveredStarIndex > rating - 1) {
       updateStars(hoveredStarIndex);
     }
-  });
+  })     //Quando il mouse passa sopra una stella, ne controlla l'indice e aggiorna le stelle visualizzate se la stella su cui si trova il mouse ha un indice superiore alla valutazione corrente.
 
   star.addEventListener("click", (event) => {
     const clickedStarIndex = parseInt(event.target.getAttribute("data-index"));
@@ -17,13 +17,12 @@ stars.forEach((star) => {
     updateStars(clickedStarIndex);
   });
 });
+// Quando si fa clic su una stella, viene impostata la valutazione sull'indice della stella cliccata + 1 (perché chiaramente parte da indice 0) e vengono aggiornate di conseguenza le stelle visualizzate.
 
-// Se il cursore va fuori dal div con id "stars"
+
 divStars.addEventListener("mouseout", function() {
-  // Se non è stata selezionata alcuna stella, esse diventano tutte vuote (per effetto di updateStars(-1))
-  // Se invece è stata già selezionata una stella, esse diventano piene solo fino alla stella selezionata
     updateStars(rating - 1);
-});
+});   // Se il cursore va fuori dal div con id "stars", richiama updateStars con il parametro rating - 1
 
 function updateStars(hoveredStarIndex) {
   stars.forEach((star, index) => {
@@ -34,3 +33,5 @@ function updateStars(hoveredStarIndex) {
     }
   });
 }
+
+//Aggiorniamo la rappresentazione visiva delle stelle in base al dato hoveredStarIndex. Itero attraverso ogni stella, cambiando l'origine dell'immagine in una stella piena o vuota, a seconda che l'indice della stella sia inferiore o uguale all'indice al momento del passaggio del mouse.
