a7fa8401a8cc05f7af1327e1266feb9515e25362
Riccardo Marchetti <marchettiriccardo04@gmail.com>
2023-12-14 18:03:07 +0100

    aggiornamento finale(in teoria)

--- script/checkbox.js
+++ script/checkbox.js
@@ -1,8 +1,15 @@
-const checkboxElement = document.getElementById('checkbox');
-const submitButton = document.getElementById('proceed');
+const checkboxElement = document.getElementById("checkbox");
+const submitButton = document.getElementById("proceed");
+const pageForm = document.getElementsByTagName("form")[0];
 
-submitButton.addEventListener('click', function(event) {
-    if (!checkboxElement.checked) {
-      alert('Please agree to the terms and conditions!');
-    }
-  });
+pageForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
+
+submitButton.addEventListener("click", function (event) {
+  if (!checkboxElement.checked) {
+    alert("Please agree to the terms and conditions!");
+  } else {
+    window.location.href = "benchmark.html";
+  }
+});
